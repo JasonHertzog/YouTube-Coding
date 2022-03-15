06e61acec362a53c51ef796e71b0dd560914e21b
JasonHertzog <86388948+JasonHertzog@users.noreply.github.com>
2022-03-15 16:36:17 -0400

    Create NameFormatting.java
    Name Formatting Problem Statement setup.

---
+++ Java Beginner/NameFormatting.java
@@ -0,0 +1,14 @@
+// Given a first name, middle name, and last name: format the name to be last name, first initial and middle initial.
+// EXAMPLE:                      CORNER CASE: Only given First and Last name
+// Input: Jason Skilled Apple          Input: Jason Apple
+// Output: Apple, J. S.               Output: Apple, J.
+
+
+import java.util.Scanner;
+
+public class NameFormatting {
+   public static void main(String [] args) {
+      
+      
+   }
+}
